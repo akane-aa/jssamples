f1b878c6993141522982586186c17640ccaf4c21
akane-aa <kagemiyasenn@gmail.com>
2019-09-07 21:07:40 +0900

    add chap04

---
+++ chap04/assignmentOperation.js
@@ -0,0 +1,13 @@
+let num = 255;
+console.log("numの値:" + num);
+
+num = num + 5;
+console.log("numの値:" + num);
+
+num += 5;
+console.log("numの値:" + num);
+
+let PI = 10;
+
+num = num + PI;
+console.log("numの値:" + num);
---
+++ chap04/remainderOperation.js
@@ -0,0 +1,5 @@
+let num1 = 3;
+let num2 = 2;
+
+let ans = num1 ** num2;
+console.log(ans);
