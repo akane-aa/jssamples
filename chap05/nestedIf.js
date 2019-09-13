f190946cc0965358bbf5e17a8fb1e75416ee355c
akane-aa <kagemiyasenn@gmail.com>
2019-09-13 19:34:52 +0900

    add new files

---
+++ chap05/nestedIf.js
@@ -0,0 +1,10 @@
+let num = Math.round(Math.rondom() * 100);
+console.log("現在の値:" + num);
+if (num % 2 === 0) {
+  console.log("2の倍数です");
+  if (num % 3 === 0) {
+    log("3の倍数です");
+  }
+} else {
+  log("2の倍数ではありません");
+}
